a67c51cbad0a3ca3ca3ebae315e32e51ec8a70dd
SR_team <sr-tream@yandex.ru>
2017-01-10 23:13:46 +0300

    upd

--- ExecProc4D.h
+++
@@ -1,4 +0,0 @@
-#pragma comment(lib, "ExecProc4D.lib")
-
-/*extern "C"*/ __declspec(dllimport)
-void CALLBACK ConnectServer( const char* nick, const char* ip, unsigned short port, const char* gta_sa, const char* samp );
